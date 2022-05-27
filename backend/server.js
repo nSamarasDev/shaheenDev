40975500633190cb656d813b711de2d9f4884d51
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-27 11:35:08 -0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const morgan = require('morgan');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Shaheen admistration API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
