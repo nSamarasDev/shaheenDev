e9745ef4ed3325613b60de23fcb8671cc50ae7cf
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-27 17:08:51 -0400

    Hook register form to redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { toast, Toast } from 'react-toastify'
 import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -12,6 +14,10 @@ function Register() {
     })
 
     const {name, email, password, password2} = formData
+
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -25,6 +31,14 @@ function Register() {
 
         if(password !== password2) {
             toast.error('passwords do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            }
+
+            dispatch(register(userData))
         }
     }
       
@@ -33,7 +47,7 @@ function Register() {
     <>
         <section className='heading'>
             <h1>
-                <FaUser /> Register
+                <FaUser /> Register {user}
             </h1>
             <p>Please create an account</p>
         </section>
