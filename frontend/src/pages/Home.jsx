1d2c48e7f3c8d83da2e53cb108d37405dc8163e4
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-27 17:07:13 -0400

    Home screen update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
             <FaQuestionCircle /> Create New Ticket
         </Link>
 
-        <Link to='/tickets' className='btn btn-reverse btn-block'>
+        <Link to='/tickets' className='btn btn-block'>
             <FaTicketAlt /> View My Tickets
         </Link>
     </>
