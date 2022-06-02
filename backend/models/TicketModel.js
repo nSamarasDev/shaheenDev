46f3932300dd536832a2f83a85c8ea7a0c10acf9
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-02 13:53:09 -0400

    TicketModel.js added

---
+++ backend/models/TicketModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    product: {
+      type: String,
+      required: [true, 'Please add a product'],
+      enum: ['iPhone', 'Mackbook Pro', 'iMac', 'ipad'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description of the issue'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Ticket', ticketSchema);
