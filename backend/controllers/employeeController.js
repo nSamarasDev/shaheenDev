3d655ed5ff9a429922f71e12f91789db21f01698
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-02 19:08:39 -0400

    employController working

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -7,14 +7,73 @@ const Employee = require('../models/EmployeeModel');
 // @route  GET /api/employee
 //@access  Private
 const getEmployee = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getEmployee' });
+  // Get user using id in jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const employee = await Employee.find({ user: req.user.id });
+
+  res.status(200).json(employee);
 });
 
 // @dec    Create new Employee
 // @route  POST /api/employee
 //@access  Private
 const createEmployee = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createEmployee' });
+  const {
+    firstName,
+    middleName,
+    lastName,
+    email,
+    socialSecurityNumber,
+    driversLicenseNumber,
+    licenseExpireDate,
+    phoneNumber,
+    address,
+  } = req.body;
+
+  if (
+    !firstName ||
+    !middleName ||
+    !lastName ||
+    !email ||
+    !socialSecurityNumber ||
+    !driversLicenseNumber ||
+    !licenseExpireDate ||
+    !phoneNumber ||
+    !address
+  ) {
+    res.status(400);
+    throw new Error('Please add employee information');
+  }
+
+  // Get user using id in jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const employee = await Employee.create({
+    firstName,
+    middleName,
+    lastName,
+    email,
+    socialSecurityNumber,
+    driversLicenseNumber,
+    licenseExpireDate,
+    phoneNumber,
+    address,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(employee);
 });
 
 module.exports = {
