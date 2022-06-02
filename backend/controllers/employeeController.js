66bc1ad3133256721f63447a6d9b02e71b83d0ad
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-02 13:48:56 -0400

    employeeController.js added

---
+++ backend/controllers/employeeController.js
@@ -0,0 +1,23 @@
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
+const Employee = require('../models/EmployeeModel');
+
+// @dec    Get Employee
+// @route  GET /api/employee
+//@access  Private
+const getEmployee = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'getEmployee' });
+});
+
+// @dec    Create new Employee
+// @route  POST /api/employee
+//@access  Private
+const createEmployee = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'createEmployee' });
+});
+
+module.exports = {
+  getEmployee,
+  createEmployee,
+};
