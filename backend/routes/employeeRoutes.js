9cae802e77a4a902a0c5e12eca9c2a04b791f242
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-03 16:19:05 -0400

    routes updated

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getEmployee,
+  getEmployees,
   createEmployee,
+  getEmployee,
 } = require('../controllers/employeeController');
 
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(protect, getEmployee).post(protect, createEmployee);
+router.route('/').get(protect, getEmployees).post(protect, createEmployee);
+
+router.route('/:id').get(protect, getEmployee);
 
 module.exports = router;
