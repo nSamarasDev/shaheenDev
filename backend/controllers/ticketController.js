b34582fd8a79a597b0233edd53f9623fae359f11
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-03 16:16:06 -0400

    controller update

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -49,7 +49,35 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+// @dec    Get user ticket
+// @route  GET /api/tickets/:id
+//@access  Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using id in jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
 module.exports = {
   getTickets,
   createTicket,
+  getTicket,
 };
