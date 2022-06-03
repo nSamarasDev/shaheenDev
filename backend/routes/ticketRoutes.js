85a1a749640c79446248f2a07ecc4547392e2d1b
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-03 18:35:18 -0400

    ticket routes.js updated

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,12 +4,18 @@ const {
   getTickets,
   createTicket,
   getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 
-router.route('/:id').get(protect, getTicket);
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
