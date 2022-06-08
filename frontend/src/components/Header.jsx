010b461c74fc55fcc63961c536be112f489fa55f
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-08 10:09:23 -0400

    header update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>Support Desk</Link>
+        <Link to='/'>Shaheen Transport</Link>
       </div>
       <ul>
         {user ? (
